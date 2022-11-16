8d14577fe00cb8013aab497426c16514e9711c44
gharjare <gharjarechetan@gmail.com>
2022-11-16 20:40:30 +0530

    Add census analyse

---
+++ CensusAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using IndianStatesAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath,string dataHeader)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeader);
+            return dataMap;
+        }
+    }
+}
